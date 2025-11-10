d9094d11e527aaded8847a7c235e03b370828f79
Stian Sundkvist <stian.sundkv@gmail.com>
2025-11-10 14:29:32 +0000

    add data for two senders

--- mininet/topologies/topology2.py
+++ mininet/topologies/topology2.py
@@ -26,42 +26,5 @@ class SimpleTopology(Topo):
         self.addLink(h2, s1, cls=TCLink, bw=50, delay="50ms", max_queue_size=25, fq_rate="10Mbit")
         self.addLink(h3, s1, cls=TCLink, bw=50, delay="50ms", max_queue_size=25, fq_rate="10Mbit")
 
-
-def run():
-    topo = SimpleTopology()
-    net = Mininet(topo=topo, link=TCLink, controller=None, autoSetMacs=True, autoStaticArp=True)
-    net.start()
-
-    for h in net.hosts:
-        h.cmd(
-            'xterm -T {n} -e bash -lc '
-            '"hostname {n}; '
-            'PS1=\'\\u@{n}:\\w# \'; '
-            'echo --- {n} ---; '
-            'ip -brief addr; '
-            'exec bash" &'.format(n=h.name)
-        )
-
-    # (Example) Start iperf server on h2, client on h1
-    h1, h2, h3 = net.get('h1', 'h2', 'h3')
-
-    multiplier = 10
-
-    h1.cmd('sudo sysctl -w net.ipv4.tcp_ss_pacing_multiplier=', multiplier)
-    h2.cmd('sudo sysctl -w net.ipv4.tcp_ss_pacing_multiplier=', multiplier)
-    h3.cmd('sudo sysctl -w net.ipv4.tcp_ss_pacing_multiplier=', multiplier)
-
-    h3.popen('iperf -s', shell=True)
-
-    print(h1.name, 'iperf ->', h1.cmd('iperf -c %s -t 30' % h3.IP()))
-    print(h2.name, 'iperf ->', h2.cmd('iperf -c %s -t 30' % h3.IP()))
-
-    CLI(net)
-    net.stop()
-
 # Topology entry point
 topos = {'mytopo': SimpleTopology}
-
-
-if __name__ == '__main__':
-    run()
