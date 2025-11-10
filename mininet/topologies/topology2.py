13710a7481b1db049105484774c8740a206973cf
Stian Hallerud Sundkvist <stian.sundkv@gmail.com>
2025-11-10 14:37:07 +0100

    set multiplier in mininet script

--- mininet/topologies/topology2.py
+++ mininet/topologies/topology2.py
@@ -36,18 +36,24 @@ def run():
     net = Mininet(topo=topo, link=TCLink, controller=None, autoSetMacs=True, autoStaticArp=True)
     net.start()
 
+    multiplier = 10
+
     for h in net.hosts:
         print(h.name, '->', h.cmd('ip -brief addr show'))
+        h.cmd('sudo sysctl -w net.ipv4.tcp_ss_pacing_multiplier=', multiplier)
 
     for h in net.hosts:
         # Keep the terminal open by ending with 'exec bash'
         h.cmd('xterm -T %s -e bash -lc "echo --- %s ---; hostname; ip -brief addr; exec bash" &' % (h.name, h.name))
 
     h1, h2, h3 = net.get('h1', 'h2', 'h3')
-   
+
+    for h in net.hosts:
+        h.cmd('sudo sysctl net.ipv4.tcp_ss_pacing_multiplier')
+
     print(h3.name, 'iperf ->', h3.cmd('iperf -s1'))
-    print(h1.name, 'iperf ->', h1.cmd('iperf -c %s -t 3' % h3.IP()))
-    print(h2.name, 'iperf ->', h1.cmd('iperf -c %s -t 3' % h3.IP()))
+    print(h1.name, 'iperf ->', h1.cmd('iperf -c %s -t 30' % h3.IP()))
+    print(h2.name, 'iperf ->', h1.cmd('iperf -c %s -t 30' % h3.IP()))
 
     # Drop to interactive CLI when you’re ready
     CLI(net)
