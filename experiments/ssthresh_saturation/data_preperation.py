c879badacd2f2c9e0995d7c070c53a349029b63d
Stian Hallerud Sundkvist <stian.sundkv@gmail.com>
2025-12-12 17:35:00 +0100

    start filtering data

---
+++ experiments/ssthresh_saturation/data_preperation.py
@@ -0,0 +1,21 @@
+
+def get_sshthresh_from_file(file_name: str):
+    first_loss = (0, None)
+    second_loss = (0, None)
+
+    with open(file_name, 'r+') as f:
+        for line in f:
+            bits = line.split(" ")
+
+            time = bits[0]
+            ssthresh = bits[12]
+
+            if ssthresh.split(":")[0] == "ssthresh":
+                if first_loss[1] is None:
+                    first_loss = (ssthresh, time)
+                elif ssthresh != first_loss[0]:
+                    second_loss = (ssthresh, time)
+                    break
+
+    return first_loss, second_loss
+
